a458275e9a847880f6defb4e5568c7670e590f24
Admin <nook09873@gmail.com>
2018-02-13 15:30:20 +0700

    Finish midterm 13Feb Waranya(nook) (พอดีโปรเจกต์หนูมัน vcs ไม่ได้ แต่ของเพื่อนมันส่งได้ เลย cone มาทำใหม่ส่งค่ะ)

--- app/src/main/java/pronchanok/krirk/ac/th/mindmidterm/CalculateFragment.java
+++ app/src/main/java/pronchanok/krirk/ac/th/mindmidterm/CalculateFragment.java
@@ -3,9 +3,11 @@ package pronchanok.krirk.ac.th.mindmidterm;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by HP on 13/2/2561.
@@ -19,6 +21,38 @@ public class CalculateFragment extends Fragment{
         return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Button button = getView().findViewById(R.id.btnExchange);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentMainFragment, new CalculateFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+        Toolbar toolbar = getView().findViewById(R.id.ToolBarCalculate);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle("Calculate USD to THB");
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    }
 
 
 }
